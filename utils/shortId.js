728bf9e5a66d3a1b3bc72f3f36bfca88dd542300
NineTwelve <ninetwelvefirstqy12@gmail.com>
2025-12-02 11:01:11 +0700

    Add basic special characters on shortId.js for UUID CDN & Shortener

--- utils/shortId.js
+++ utils/shortId.js
@@ -1,6 +1,6 @@
 // Fungsi sederhana untuk generate short ID yang lebih reliable
 function generateShortId(length = 6) {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
     let result = '';
     for (let i = 0; i < length; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
